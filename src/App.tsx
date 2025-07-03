8a6806beae2270a1f9c96178731495a5bbff8b1c
Serhii Kyrylenko <lukey868@gmail.com>
2025-07-03 23:57:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Button from './components/Button'
 const App = () => {
 
   const images = [
-    '../src/assets/img/pic1.jpg',
-    '../src/assets/img/pic2.jpg',
-    '../src/assets/img/pic3.jpg',
-    '../src/assets/img/pic4.jpg',
-    '../src/assets/img/pic5.jpg',
+    './src/assets/img/pic1.jpg',
+    './src/assets/img/pic2.jpg',
+    './src/assets/img/pic3.jpg',
+    './src/assets/img/pic4.jpg',
+    './src/assets/img/pic5.jpg',
   ];
-  const [bg, setBg] = useState<string>("../src/assets/img/pic1.jpg");
+  const [bg, setBg] = useState<string>("./src/assets/img/pic1.jpg");
   const [prevIndex, setPrevIndex] = useState<number | null>(null);
 
   const randomPictures = () => {
@@ -30,7 +30,7 @@ const App = () => {
     <div
       className="btn-background"
       style={bg ? { backgroundImage: `url(${bg})` } : {}}>
-        
+
       <Button onClick={randomPictures}>Change background</Button>
     </div>
   );
